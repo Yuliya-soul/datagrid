a0bc4441df739259d138bd3a8ac664505f1db7f5
Your Name <you@example.com>
2020-03-16 23:39:07 +0300

    feat:datagrid with Redux

---
+++ src/reducers/sortedData.js
@@ -0,0 +1,30 @@
+import { SORT_DATA } from '../actions/types';
+
+const initialState = {
+    isSorted: false,
+    data: [],
+    sort:'',
+    sortField:'rank',
+    isActive:true
+}
+
+export default (state = initialState, action) => {
+  
+    switch(action.type) {
+        case SORT_DATA: return {
+            isSorted: true,
+            data: action.payload.newData,
+            sort: action.payload.sort,
+            sortField: action.payload.sortField,
+            isActive:state.isActive
+        };
+        case "FILTERED_DATA": 
+        console.log(action.payload);
+            return {
+                ...state,
+             //   data: action.payload.newData,
+                isActive: action.payload.isActive
+            }
+        default: return state;
+    }
+}
